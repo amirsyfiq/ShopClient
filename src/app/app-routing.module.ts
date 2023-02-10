b44fd52acf1f5b30aa1cd43975d1166c93355bf9
Amir Syafiq <76594956+amirsyfiq@users.noreply.github.com>
2023-02-10 11:59:40 +0800

    Create Login, Register, Homepage component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { RegisterComponent } from './pages/register/register/register.component';
+import { HomepageComponent } from './pages/homepage/homepage/homepage.component';
+import { LoginComponent } from './pages/login/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
