f1c2a1f3a2c4cb148117bcf703733bc704137faf
Amir Syafiq <76594956+amirsyfiq@users.noreply.github.com>
2023-02-10 18:14:50 +0800

    Reorganize Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { RegisterComponent } from './pages/register/register/register.component';
-import { HomepageComponent } from './pages/homepage/homepage/homepage.component';
-import { LoginComponent } from './pages/login/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './pages/login/login/login.component';
-import { HomepageComponent } from './pages/homepage/homepage/homepage.component';
-import { RegisterComponent } from './pages/register/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HeaderComponent } from './pages/homepage/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomepageComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
 export class AppModule { }
